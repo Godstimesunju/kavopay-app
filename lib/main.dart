a2cdab3831323cc77ce1e571f01218ace383d2a7
Godstimesunju <godstimesunju@gmail.com>
2026-03-12 19:50:13 +0100

    Add InAppWebView with Google signin and downloads

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const KavopayApp());
 }
 
@@ -27,22 +29,47 @@ class WebViewScreen extends StatefulWidget {
 }
 
 class _WebViewScreenState extends State<WebViewScreen> {
-  late final WebViewController _controller;
+  InAppWebViewController? _controller;
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setUserAgent('Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36')
-      ..loadRequest(Uri.parse('https://kavopay.web.app'));
-  }
+  final InAppWebViewSettings _settings = InAppWebViewSettings(
+    javaScriptEnabled: true,
+    domStorageEnabled: true,
+    databaseEnabled: true,
+    useHybridComposition: true,
+    allowsInlineMediaPlayback: true,
+    mediaPlaybackRequiresUserGesture: false,
+    userAgent: 'Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF071427),
       body: SafeArea(
-        child: WebViewWidget(controller: _controller),
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: WebUri('https://kavopay.web.app'),
+          ),
+          initialSettings: _settings,
+          onWebViewCreated: (controller) {
+            _controller = controller;
+          },
+          shouldOverrideUrlLoading: (controller, action) async {
+            final url = action.request.url.toString();
+            if (url.startsWith('https://accounts.google.com') ||
+                url.startsWith('https://kavopay.web.app') ||
+                url.startsWith('https://kavopay.firebaseapp.com')) {
+              return NavigationActionPolicy.ALLOW;
+            }
+            await launchUrl(Uri.parse(url),
+                mode: LaunchMode.externalApplication);
+            return NavigationActionPolicy.CANCEL;
+          },
+          onDownloadStartRequest: (controller, request) async {
+            await launchUrl(Uri.parse(request.url.toString()),
+                mode: LaunchMode.externalApplication);
+          },
+        ),
       ),
     );
   }
